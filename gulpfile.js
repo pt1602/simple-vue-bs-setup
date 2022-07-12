0bb41817cb3c1914695440958bc8c36e0cb98f71
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2022-07-12 09:09:36 +0200

    feat(core): general improvements
    * added ddev
    * added font-awesome
    * moved app.js
    * update packages
    * improved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,58 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var npmDist = require('gulp-npm-dist');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const npmDist = require('gulp-npm-dist');
 
 gulp.task('copy:libs', function () {
     return gulp.src(npmDist({
-            copyUnminified: false,
-            replaceDefaultExcludes: true,
-            excludes: [
-                'src/**/*',
-                'examples/**/*',
-                'example/**/*',
-                'demo/**/*',
-                'spec/**/*',
-                'docs/**/*',
-                'tests/**/*',
-                'test/**/*',
-                'Gruntfile.js',
-                'gulpfile.js',
-                'package.json',
-                'package-lock.json',
-                'bower.json',
-                'composer.json',
-                'yarn.lock',
-                'webpack.config.js',
-                'README',
-                'LICENSE',
-                'CHANGELOG',
-                '*.yml',
-                '*.md',
-                '*.coffee',
-                '*.ts',
-                '*.less'
-            ]
-        }), {
-            base: './node_modules'
-        })
+        copyUnminified: false,
+        replaceDefaultExcludes: true,
+        excludes: [
+            'src/**/*',
+            'examples/**/*',
+            'example/**/*',
+            'demo/**/*',
+            'spec/**/*',
+            'docs/**/*',
+            'tests/**/*',
+            'test/**/*',
+            'Gruntfile.js',
+            'gulpfile.js',
+            'package.json',
+            'package-lock.json',
+            'bower.json',
+            'composer.json',
+            'yarn.lock',
+            'webpack.config.js',
+            'README',
+            'LICENSE',
+            'CHANGELOG',
+            '*.yml',
+            '*.md',
+            '*.coffee',
+            '*.ts',
+            '*.less'
+        ]
+    }), {
+        base: './node_modules'
+    })
         .pipe(gulp.dest('./public/libs'));
 });
 
 gulp.task('sass:compile', function () {
     return gulp.src('./scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('css:minify', () => {
+    return gulp.src('./public/css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./scss/**/*.scss', gulp.series('sass:compile'));
+    gulp.watch('./scss/**/*.scss', gulp.series('sass:compile', 'css:minify'));
 });
+
+gulp.task('default', gulp.series('copy:libs', 'sass:compile' , 'css:minify'));
